a3d523a9c248d5a160f1647eeb9261b269c44f26
Jason Thatcher <contact@jasonthatcher.com>
2017-11-19 20:11:40 -0600

    Class, Property, Methods,  Overloaded Constructor
    Class, Property, Methods,  Overloaded Constructor

--- RealEstateProfitCalculator/Program.cs
+++ RealEstateProfitCalculator/Program.cs
@@ -10,6 +10,40 @@ namespace RealEstateProfitCalculator
     {
         static void Main(string[] args)
         {
+            double annualTaxAmount = 0.00d;
+            double monthlyTaxAmount = 0.00d;
+            double annualInsurance = 0.00d;
+            double monthlyInsurance = 0.00d;
+
+            RealEstateInvestment myRE = new RealEstateInvestment("101 1st Street", 1993, 235647);
+            Console.WriteLine("Property Information");
+            Console.WriteLine("Address: " + myRE.StreetAddress);
+            Console.WriteLine("Year Built: " + myRE.YearBuilt.ToString());
+            Console.WriteLine("Purchase Price: " + myRE.PurchasePrice.ToString("C2"));
+
+
+            Console.WriteLine("\n");
+            Console.Write("Please Enter Income From Rent: ");
+            myRE.IncomeFromRent = double.Parse(Console.ReadLine());
+
+            Console.Write("Please Enter Annual Tax Amount: ");
+            annualTaxAmount = double.Parse(Console.ReadLine());
+            monthlyTaxAmount = annualTaxAmount / 12;
+
+            Console.Write("Please Enter Annual Insurance Amount: ");
+            annualInsurance = double.Parse(Console.ReadLine());
+            monthlyInsurance = annualInsurance / 12;
+
+            myRE.MonthlyExpense = monthlyInsurance + monthlyTaxAmount;
+
+            Console.Write("\n");
+            Console.WriteLine("Real Estate Investment Summary");
+
+            Console.WriteLine("Monthly Income: " + myRE.IncomeFromRent.ToString("C2"));
+            Console.WriteLine("Monthly Expense: " + myRE.MonthlyExpense.ToString("C2"));
+            Console.WriteLine("Monthly Earnings: " + myRE.GetMonthlyEarnings().ToString("C2"));
+
+            Console.ReadLine();
         }
     }
 }
---
+++ RealEstateProfitCalculator/RealEstateInvestment.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateProfitCalculator
+{
+    class RealEstateInvestment
+    {
+
+        private int yearBuilt;
+        private string streetAddress;
+        private double purchasePrice;
+        private double monthlyExpense;
+        private double incomeFromRent;
+
+        public int YearBuilt
+        {
+            get { return yearBuilt; }
+            set { yearBuilt = value; }
+        }
+
+        public string StreetAddress
+        {
+            get { return streetAddress; }
+            set { streetAddress = value; }
+        }
+
+        public double PurchasePrice
+        {
+            get { return purchasePrice; }
+            set { purchasePrice = value; }
+        }
+
+        public double MonthlyExpense
+        {
+            get { return monthlyExpense; }
+            set { monthlyExpense = value; }
+        }
+
+        public double IncomeFromRent
+        {
+            get { return incomeFromRent; }
+            set { incomeFromRent = value; }
+        }
+
+        public RealEstateInvestment(string address, int year, double price)
+        {
+            streetAddress = address;
+            purchasePrice = price;
+            yearBuilt = year;
+        }
+
+        public RealEstateInvestment(int year, double price)
+        {
+
+        }
+
+        public RealEstateInvestment(string address, double price)
+        {
+
+        }
+
+        public double GetMonthlyEarnings()
+        {
+            return incomeFromRent - monthlyExpense;
+        }
+    }
+}
